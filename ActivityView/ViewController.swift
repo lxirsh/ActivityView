959352aca2054f7dcd45bf387a7d575f1a4443e4
Lance Hirsch <lxirsh@gmail.com>
2015-05-16 11:23:49 +0300

    Create button and function to invoke UIActivityViewController.

--- ActivityView/ViewController.swift
+++ ActivityView/ViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LaunchActivityView: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func LaunchActivityView(sender: AnyObject) {
+        
+        let image = UIImage()
+        let nextController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        self.presentViewController(nextController, animated: true, completion: nil)
     }
-
-
 }
 
